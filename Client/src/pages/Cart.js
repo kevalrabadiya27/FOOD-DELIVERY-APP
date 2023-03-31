7f6e93f021c30c67ceb9282d3fe34b8e9670409e
keval Rabadiya <kevalrabadiya27@gmail.com>
2023-03-31 17:55:12 +0530

    "updated"

--- Client/src/pages/Cart.js
+++ Client/src/pages/Cart.js
@@ -17,7 +17,7 @@ export default function Cart() {
     }
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
-        let response = await fetch("http://localhost:8080/api/orderData", {
+        let response = await fetch("https://food-delivery-api-zdvf.onrender.com/api/orderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -28,6 +28,7 @@ export default function Cart() {
                 order_date: new Date().toDateString()
             })
         });
+        console.log(response);
         if (response.status === 200) {
             toast.success("Your Order Save in Database")
             dispatch({ type: "DROP" })
